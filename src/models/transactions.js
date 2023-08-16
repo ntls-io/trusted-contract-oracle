8a3e362c0b839e813ee8cdfc81ece78e86f8cd21
takundachirema <takunda.chirema@alumni.uct.ac.za>
2023-08-16 13:04:20 +0200

    prepare transactions to send to enclave

--- src/models/transactions.js
+++ src/models/transactions.js
@@ -1,13 +1,26 @@
 import mongoose from '../../services/db/mongoose.js';
 import { Schema } from 'mongoose';
 
+export const transactionTypes = {
+	payment: 'Payment',
+};
+
 export const accountSchema = new Schema({
 	hash: {
 		type: String,
 		required: true,
 		unique: true,
 	},
-	account: { type: String, ref: 'Account', required: true },
+	type: {
+		type: String,
+		required: true,
+	},
+	account: { type: String, ref: 'Account' },
+	sender: String,
+	recepient: String,
+	amount: String,
+	currency: String,
+	price: String,
 });
 
 const Transaction = mongoose.model(
